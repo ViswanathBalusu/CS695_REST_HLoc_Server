3c70930ddbba27b6afd13a067fe044c98aacaf67
Chandra Kiran Viswanath Balusu <cbalusu@gmu.edu>
2023-05-14 16:13:11 -0400

    Remove Unused code

--- hloc_server/__main__.py
+++ hloc_server/__main__.py
@@ -1,7 +1,1 @@
-# async def test():
-#     await models.create_all()
-#     # await UUIDS.objects.create(name="test", path="./test/", )
-#     note = await UUIDS.objects.all()
-#     print(note)
-#
-# asyncio.run(test())
+
