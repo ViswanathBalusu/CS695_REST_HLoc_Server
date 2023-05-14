c3866982e48dd7fcb4f250a17c016109e22ebb12
Chandra Kiran Viswanath Balusu <cbalusu@gmu.edu>
2023-05-14 15:50:12 -0400

    Format using PEP8 Rules

--- hloc_server/__main__.py
+++ hloc_server/__main__.py
@@ -1,8 +1,3 @@
-from hloc_server import models
-from .helpers.database import UUIDS
-import asyncio
-
-
 # async def test():
 #     await models.create_all()
 #     # await UUIDS.objects.create(name="test", path="./test/", )
--- hloc_server/routers/__init__.py
+++ hloc_server/routers/__init__.py
@@ -1,5 +1,5 @@
 from .dataset_upload import DataSetRouter
 from .localization_route import LocalizeRouter
 from .map_generation_route import MapRouter
-from .session_route import SessionRouter
 from .point_cloud_stream import PointCloudRouter
+from .session_route import SessionRouter
